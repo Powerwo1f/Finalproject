bf6ca2f2e67aafc0e0bf3e5180058cd3469d6a9c
Powerwo1f <belous.aleksei18@gmail.com>
2020-07-10 19:14:00 +0300

    none

---
+++ js/newScript.js
@@ -0,0 +1,6 @@
+const fun = () => {
+    console.log('hi')
+}
+module.exports = {
+    fun : fun()
+}
---
+++ js/script.js
@@ -0,0 +1,15 @@
+import css from '../css/main.css'
+import files from './newScript'
+let elem = document.createElement('p')
+elem.attachShadow({mode : 'open' })
+elem.id = '1'
+elem.style = `
+    border : 1px solid black;
+    height : 100px;
+    width : 100px;
+`
+var z = document.createElement('p'); // is a node
+z.innerHTML = 'test satu dua tiga';
+
+document.body.appendChild(elem)
+elem.appendChild(z)
